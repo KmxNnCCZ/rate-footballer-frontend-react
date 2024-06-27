8ff3ac3428c88156757b2b30b3c129415c1f4231
ああああああ <mmmm48698062@icloud.com>
2024-06-28 00:14:04 +0900

    採点投稿ページの作成

--- src/routes/Rate.js
+++ src/routes/Rate.js
@@ -7,26 +7,35 @@ import {
   Flex,
   Image,
   Heading,
-  Table,
-  Tbody,
   Button,
-  Tr,
-  Td,
-  Stack,
-  Divider,
-  border
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Textarea,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 
 import { getTeam } from "../lib/api/getMatch";
 import { Loading } from "../components/Loading";
 import positionShortName from "../lib/PositionShortNames";
 
+import { getUser } from "../lib/api/auth";
+import { postRate } from "../lib/api/postRate";
+
+
 export const Rate = () => {
   const { matchApiId } = useParams();
   const [teamData, setTeamData] = useState({});
   const [loading, setLoading] = useState(true);
 
-  const [scores, setScores] = useState([...Array(11)].map(() => "5.0"));
+  const [playerRates, setPlayerRates] = useState([...Array(11)].map(() => ({
+    playerApiId: undefined,
+    score: "5.0",
+    assessment: undefined
+  })));
 
   // パラメータを取得
   const query = new URLSearchParams(useLocation().search);
@@ -40,6 +49,14 @@ export const Rate = () => {
         const res = await getTeam(matchApiId, team);
         // console.log(JSON.stringify(res.data, null, 2))
         setTeamData(res.data);
+        res.data.lineup.forEach((player, i) => {
+          setPlayerRates(prevPlayerRates => {
+            const newPlayerRates = [...prevPlayerRates];
+            newPlayerRates[i].playerApiId = player.id
+            return newPlayerRates;
+          })
+        })
+
         setLoading(false); // データの読み込みが完了
       } catch (error) {
         console.error("Error fetching match:", error);
@@ -51,38 +68,50 @@ export const Rate = () => {
   }, [matchApiId, team]);
 
   const increment = (i) => {
-    setScores((prevScores) => {
-      const newScores = [...prevScores];
-      newScores[i] = formatToOneDecimal(Math.min(10, Number(newScores[i]) + 0.5)); // 最高値を10とする
-      return newScores;
+    setPlayerRates((prevPlayerRates) => {
+      const newPlayerRates = [...prevPlayerRates];
+      newPlayerRates[i].score = formatToOneDecimal(Math.min(10, Number(newPlayerRates[i].score) + 0.5)); // 最高値を10とする
+      return newPlayerRates;
     });
   }
 
   const decrement = (i) => {
-    setScores((prevScores) => {
-      const newScores = [...prevScores];
-      newScores[i] = formatToOneDecimal(Math.max(0, Number(newScores[i]) - 0.5)); // 最低値を0とする
-      return newScores;
+    setPlayerRates((prevPlayerRates) => {
+      const newPlayerRates = [...prevPlayerRates];
+      newPlayerRates[i].score = formatToOneDecimal(Math.max(0, Number(newPlayerRates[i].score) - 0.5)); // 最低値を0とする
+      return newPlayerRates;
     });
-  }
-
-  const handleScoreChange = (i, value) => {
-    if (!isNaN(value)) { // 数値に変換できる場合のみ更新する
-      setScores((prevScores) => {
-        const newScores = [...prevScores];
-        newScores[i] = value;
-        return newScores;
-      });
-    }
   }
 
   const formatToOneDecimal = (value) => {
     return String((value).toFixed(1));
   }
 
-  // const isHalfStep = (number) => {
-  //   return (number * 10) % 5 === 0;
-  // }
+  const changeAssessment = (i, value) => {
+    setPlayerRates((prevPlayerRates) => {
+      const newPlayerRates = [...prevPlayerRates];
+      prevPlayerRates[i].assessment = value;
+      return newPlayerRates;
+    })
+  }
+
+  const postRateData = () => {
+    // ログインしているか確認
+    const f = async () => {
+      try {
+        const res = await getUser();
+	      if (res && res.data.isLogin) {
+          postRate(matchApiId, team, playerRates);
+        } else {
+          // ログインを促す処理
+          console.log("未ログイン");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    f();
+  }
   
 
   // データが読み込まれるまでローディングを表示
@@ -122,38 +151,70 @@ export const Rate = () => {
         </Box>
       </Flex>
 
-      <Box mb="20px" borderColor="#22543D" borderBottomWidth="2px">
-      <Text fontSize="sm" as="b">{teamData.name} スターティングメンバー</Text>
-    </Box>
+      <Box mb="20px" borderColor="#89DA59" borderBottomWidth="2px">
+        <Text fontSize="sm" as="b">{teamData.name} スターティングメンバー</Text>
+      </Box>
 
-      <Box boxShadow="0px 2px 8px rgba(0, 0, 0, 0.1)"> {/* 投稿場所 */}
-        <Table size='sm'>
-          <Tbody> {/*Trをmapで表示 */}
-            {teamData.lineup.map((player, i) => (
-              <Tr key={player.id}>
-                <Td>{positionShortName[player.position]}</Td>
-                <Td>{player.shirtNumber}</Td>
-                <Td>{player.name}</Td>
-                <Td> {/* スコア操作 */}
-                  <Flex alignItems="center">
-                    <Button colorScheme='blue' borderWidth="2px" variant='outline' onClick={() => decrement(i)}>-</Button>
-                    <Text
-                      as='b'
-                      mx="20px"
-                      textAlign="center"
-                      onChange={(e) => handleScoreChange(i, e.target.value)}
-                    >
-                      {scores[i]}
-                    </Text>
-                    <Button colorScheme='red' variant='outline' borderWidth="2px" onClick={() => increment(i)}>+</Button>
-                  </Flex>
-                </Td>
-                <Td><Button>評価を追加</Button></Td>{/* アコーディオン */}
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-        <Button></Button>
+      <Box boxShadow="0px 2px 8px rgba(0, 0, 0, 0.1)" textAlign="center"> {/* 投稿場所 */}
+        <Box>
+          <Accordion allowMultiple>
+              {teamData.lineup.map((player, i) => (
+                <AccordionItem key={player.id}>
+                  <Grid templateColumns="1fr 1fr 5fr 3fr 3fr" alignItems="center" my="10px">
+                    <GridItem>{positionShortName[player.position]}</GridItem>
+                    <GridItem>{player.shirtNumber}</GridItem>
+                    <GridItem>{player.name}</GridItem>
+                    <GridItem mx="auto"> {/* スコア操作 */}
+                      <Flex alignItems="center">
+                        <Button colorScheme='blue' borderWidth="2px" variant='outline' onClick={() => decrement(i)}>-</Button>
+                        <Text
+                          as='b'
+                          mx="20px"
+                          textAlign="center"
+                        >
+                          {playerRates[i].score}
+                        </Text>
+                        <Button colorScheme='red' variant='outline' borderWidth="2px" onClick={() => increment(i)}>+</Button>
+                      </Flex>
+                    </GridItem>
+                    <GridItem>
+                      <AccordionButton>
+                        <Box as='b'>
+                          評価を追加
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </GridItem>
+                  </Grid>
+                  <AccordionPanel>
+                    <Textarea
+                      borderWidth="2.5px"
+                      _focus={{
+                        outline: "none"
+                      }}
+                      value={playerRates[i].assessment}
+                      onChange={(e) => changeAssessment(i, e.target.value)}
+                    />
+                  </AccordionPanel>
+                </AccordionItem>
+              ))}
+          </Accordion>
+        </Box>
+        <Button 
+        as='button'
+        my="20px"
+        w="50%"
+        h="70px"
+        color="#89DA59"
+        bg="white"
+        borderColor='#89DA59'
+        borderRadius="50px"
+        borderWidth="4px"
+        _hover={{ bg: '#89DA59', color: "white" }}
+        onClick={postRateData}
+      >
+        投稿する
+      </Button>
       </Box>
     </Box>
     
