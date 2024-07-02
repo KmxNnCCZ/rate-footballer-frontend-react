9b9eddde0ffcf42bfcfd8198697ebbb154dca133
ああああああ <mmmm48698062@icloud.com>
2024-07-02 21:00:59 +0900

    プライバシーポリシーページの作成

--- src/routes/TermsOfUse.js
+++ src/routes/TermsOfUse.js
@@ -24,7 +24,7 @@ const Article = ({ children }) => {
   )
 }
 
-const ArticleBox = ({ children }) => {
+const ArticleText = ({ children }) => {
   return (
     <Box mt="10px" w="90%" mx="auto">
       {children}
@@ -62,50 +62,50 @@ export const TermOfUse = () => {
       <Heading mb="40px">利用規約</Heading>
 
       <Text mb="30px" >
-        この利用規約（以下，「本規約」といいます。）は，Rate-Footballer（以下，「運営者」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。
+        この利用規約（以下，「本規約」といいます。）は，運営者がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。
       </Text>
 
-    <VStack spacing='50px'>
+    <VStack spacing='50px' align="flex-start">
       <Box>
         <Article>第1条（適用）</Article>
-        <ArticleBox>
-            <ParentUnorderedList spacing={2}>
-              <ListItem>
-                本規約は，ユーザーと当社との間の本サービスの利用に関わる一切の関係に適用されるものとします。
-              </ListItem>
-              <ListItem>
-                当社は本サービスに関し，本規約のほか，ご利用にあたってのルール等，各種の定め（以下，「個別規定」といいます。）をすることがあります。これら個別規定はその名称のいかんに関わらず，本規約の一部を構成するものとします。
-              </ListItem>
-              <ListItem>
-                本規約の規定が前条の個別規定の規定と矛盾する場合には，個別規定において特段の定めなき限り，個別規定の規定が優先されるものとします。
-              </ListItem>
-            </ParentUnorderedList>
-          </ArticleBox>
+        <ArticleText>
+          <ParentUnorderedList>
+            <ListItem>
+              本規約は，ユーザーと当社との間の本サービスの利用に関わる一切の関係に適用されるものとします。
+            </ListItem>
+            <ListItem>
+              当社は本サービスに関し，本規約のほか，ご利用にあたってのルール等，各種の定め（以下，「個別規定」といいます。）をすることがあります。これら個別規定はその名称のいかんに関わらず，本規約の一部を構成するものとします。
+            </ListItem>
+            <ListItem>
+              本規約の規定が前条の個別規定の規定と矛盾する場合には，個別規定において特段の定めなき限り，個別規定の規定が優先されるものとします。
+            </ListItem>
+          </ParentUnorderedList>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第2条（利用登録）</Article>
-        <ArticleBox>
-            <ParentUnorderedList spacing={2}>
-              <ListItem>
-                本サービスにおいては，登録希望者が本規約に同意の上，当社の定める方法によって利用登録を申請し，当社がこの承認を登録希望者に通知することによって，利用登録が完了するものとします。
-                </ListItem>
-              <ListItem>
-                当社は，利用登録の申請者に以下の事由があると判断した場合，利用登録の申請を承認しないことがあり，その理由については一切の開示義務を負わないものとします。
+        <ArticleText>
+          <ParentUnorderedList>
+            <ListItem>
+              本サービスにおいては，登録希望者が本規約に同意の上，当社の定める方法によって利用登録を申請し，当社がこの承認を登録希望者に通知することによって，利用登録が完了するものとします。
               </ListItem>
-                <ChildUnorderedList>
-                  <ListItem>利用登録の申請に際して虚偽の事項を届け出た場合</ListItem>
-                  <ListItem>本規約に違反したことがある者からの申請である場合</ListItem>
-                  <ListItem>その他，当社が利用登録を相当でないと判断した場合</ListItem>
-                </ChildUnorderedList>
-              </ParentUnorderedList>
-          </ArticleBox>
+            <ListItem>
+              当社は，利用登録の申請者に以下の事由があると判断した場合，利用登録の申請を承認しないことがあり，その理由については一切の開示義務を負わないものとします。
+            </ListItem>
+              <ChildUnorderedList>
+                <ListItem>利用登録の申請に際して虚偽の事項を届け出た場合</ListItem>
+                <ListItem>本規約に違反したことがある者からの申請である場合</ListItem>
+                <ListItem>その他，当社が利用登録を相当でないと判断した場合</ListItem>
+              </ChildUnorderedList>
+            </ParentUnorderedList>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第3条（ユーザーIDおよびパスワードの管理）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               ユーザーは，自己の責任において，本サービスのユーザーIDおよびパスワードを適切に管理するものとします。
             </ListItem>
@@ -116,13 +116,13 @@ export const TermOfUse = () => {
               ユーザーID及びパスワードが第三者によって使用されたことによって生じた損害は，当社に故意又は重大な過失がある場合を除き，当社は一切の責任を負わないものとします。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第4条（禁止事項）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>法令または公序良俗に違反する行為</ListItem>
             <ListItem>犯罪行為に関連する行為</ListItem>
             <ListItem>
@@ -174,13 +174,13 @@ export const TermOfUse = () => {
               </ChildUnorderedList>
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第5条（本サービスの提供の停止等）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               当社は，以下のいずれかの事由があると判断した場合，ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
               <ChildUnorderedList>
@@ -194,13 +194,13 @@ export const TermOfUse = () => {
               当社は，本サービスの提供の停止または中断により，ユーザーまたは第三者が被ったいかなる不利益または損害についても，一切の責任を負わないものとします。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第6条（著作権）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               ユーザーは，自ら著作権等の必要な知的財産権を有するか，または必要な権利者の許諾を得た文章，画像や映像等の情報に関してのみ，本サービスを利用し，投稿ないしアップロードすることができるものとします。
             </ListItem>
@@ -211,13 +211,13 @@ export const TermOfUse = () => {
               前項本文の定めるものを除き，本サービスおよび本サービスに関連する一切の情報についての著作権およびその他の知的財産権はすべて当社または当社にその利用を許諾した権利者に帰属し，ユーザーは無断で複製，譲渡，貸与，翻訳，改変，転載，公衆送信（送信可能化を含みます。），伝送，配布，出版，営業使用等をしてはならないものとします。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第7条（利用制限および登録抹消）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               当社は，ユーザーが以下のいずれかに該当する場合には，事前の通知なく，投稿データを削除し，ユーザーに対して本サービスの全部もしくは一部の利用を制限しまたはユーザーとしての登録を抹消することができるものとします。
               <ChildUnorderedList>
@@ -237,20 +237,20 @@ export const TermOfUse = () => {
               当社は，本条に基づき当社が行った行為によりユーザーに生じた損害について，一切の責任を負いません。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第8条（退会）</Article>
-       <ArticleBox>
+        <ArticleText>
           ユーザーは，当社の定める退会手続により，本サービスから退会できるものとします。
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第9条（保証の否認および免責事項）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               当社は，本サービスに事実上または法律上の瑕疵（安全性，信頼性，正確性，完全性，有効性，特定の目的への適合性，セキュリティなどに関する欠陥，エラーやバグ，権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。
             </ListItem>
@@ -264,20 +264,20 @@ export const TermOfUse = () => {
               当社は，本サービスに関して，ユーザーと他のユーザーまたは第三者との間において生じた取引，連絡または紛争等について一切責任を負いません。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第10条（サービス内容の変更等）</Article>
-       <ArticleBox>
+        <ArticleText>
           当社は，ユーザーへの事前の告知をもって，本サービスの内容を変更、追加または廃止することがあり、ユーザーはこれを承諾するものとします。
-        </ArticleBox>
+        </ArticleText>
       </Box>
     
       <Box>
         <Article>第11条（利用規約の変更）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               当社は以下の場合には、ユーザーの個別の同意を要せず、本規約を変更することができるものとします。
             </ListItem>
@@ -293,34 +293,34 @@ export const TermOfUse = () => {
               当社はユーザーに対し、前項による本規約の変更にあたり、事前に、本規約を変更する旨及び変更後の本規約の内容並びにその効力発生時期を通知します。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第12条（個人情報の取扱い）</Article>
-       <ArticleBox>
+        <ArticleText>
           当社は，本サービスの利用によって取得する個人情報については，当社「プライバシーポリシー」に従い適切に取り扱うものとします。
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第13条（通知または連絡）</Article>
-       <ArticleBox>
+        <ArticleText>
           ユーザーと当社との間の通知または連絡は，当社の定める方法によって行うものとします。当社は,ユーザーから,当社が別途定める方式に従った変更届け出がない限り,現在登録されている連絡先が有効なものとみなして当該連絡先へ通知または連絡を行い,これらは,発信時にユーザーへ到達したものとみなします。
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第14条（権利義務の譲渡の禁止）</Article>
-       <ArticleBox>
+        <ArticleText>
           ユーザーは，当社の書面による事前の承諾なく，利用契約上の地位または本規約に基づく権利もしくは義務を第三者に譲渡し，または担保に供することはできません。
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
         <Article>第15条（準拠法・裁判管轄）</Article>
-       <ArticleBox>
-          <ParentUnorderedList spacing={2}>
+        <ArticleText>
+          <ParentUnorderedList>
             <ListItem>
               本規約の解釈にあたっては，日本法を準拠法とします。
             </ListItem>
@@ -328,7 +328,7 @@ export const TermOfUse = () => {
               本サービスに関して紛争が生じた場合には，当社の本店所在地を管轄する裁判所を専属的合意管轄とします。
             </ListItem>
           </ParentUnorderedList>
-        </ArticleBox>
+        </ArticleText>
       </Box>
 
       <Box>
